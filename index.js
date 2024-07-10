3a4d77244a0af7401d6d703955d3731c9079c97b
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-07-10 18:24:33 +0530

    learning sql integration with node

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
 
-const connection =  mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'nodewithsql',
-    password: "ranajihariomj@1"
+    database: 'learnsql',
+    password: 'ranajihariom@1'
 });
 
-connection.query("SHOW TABLES", (err, result) => {
+let createRandomUser = () => {
+
+    return [
+        faker.datatype.uuid(),
+        faker.internet.userName(),
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
+}
+
+let query = "INSERT INTO user  (id,username,email,password) VALUES ?";
+
+let users = [];
+for (let i = 0; i < 10; i++) {
+    users.push(createRandomUser());
+}
+
+connection.query(query, [users], (err, result) => {
     if (err) {
         console.error('Error showing tables:', err);
         return;
-    }   
+    }
     console.log(result);
 });
 
 connection.end();
-let createRandomUser = () => {
-    return {
-        userId: faker.datatype.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        avatar: faker.image.avatar(),
-        password: faker.internet.password(),
-        birthdate: faker.date.birthdate({ min: 1900, max: 2000 }).toISOString().split('T')[0], // Format as YYYY-MM-DD
-        registeredAt: faker.date.past().toISOString().split('T')[0], // Format as YYYY-MM-DD
-    };
-}
 
-console.log(createRandomUser());
---
+++ schema.sql
@@ -0,0 +1,10 @@
+create table user(
+
+    id varchar(50) primary key,
+    username varchar(50) unique,
+    email varchar(50) unique not null,
+    password varchar(50) not null
+);
+//database is learnsql
+//tables are user and learnsql
+//current table user
