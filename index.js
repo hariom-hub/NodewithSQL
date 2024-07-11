0d33ebca2979a4d529e00aedd1611d3298af74b0
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-07-12 00:07:59 +0530

    grinding on backend with  db(mysql)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mysql = require("mysql2");
 const express = require("express");
 const app = express();
 const path = require("path");
+const { error } = require('console');
+
 app.set("views engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -25,31 +27,75 @@ app.get('/', (req, res) => {
 
     try {
 
-        connection.query(query, (error,result) => {
+        connection.query(query, (error, result) => {
 
             if (error) {
                 res.send(error);
             }
-                console.log("query is working properly");
-                console.log(result[0]["count(*)"]);
-                // let {count} = result[0]["count(*)"];
-                // res.render("home.ejs",{count});
-                let count = [];
-                count[0] = result[0]["count(*)"];
-                res.render("home.ejs",{count});
+            console.log("query is working properly");
+            console.log(result[0]["count(*)"]);
+            // let {count} = result[0]["count(*)"];
+            // res.render("home.ejs",{count});
+
+            let count = result[0]["count(*)"];
+            console.log(result[0]["id"])
+            res.render("home.ejs", { count });
         })
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }
-})
+});
 
+//show user
+
+app.get('/user', (req, res) => {
+
+    let query = `select * from user`;
+
+
+    try {
+        connection.query(query, (error, users) => {
+
+            if (error) {
+                throw error;
+            }
+            res.render("showUser.ejs", { users });
+        })
+    } catch (error) {
+
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+//edit username
+
+app.get('/user/:id/edit', (req, res) => {
+
+    let { id } = req.params;
+    let query = `select * from user where id = '${id}'`;
+    try {
+        connection.query(query, (error, result) => {
+
+            if (error) {
+                throw error;
+            }
+            console.log(result);
+            res.render("edit.ejs", { result });
+        })
+    } catch (error) {
+        res.sendStatus(500);
+    }
+    // console.log(id);
+});
 
 app.listen(port, () => {
 
     console.log(`server is listening to the port : ${port}`);
 
 });
+
 // let createRandomUser = () => {
 
 //     return [
